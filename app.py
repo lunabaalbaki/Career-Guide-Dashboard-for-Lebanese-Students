d84df4d5f726c0f13d7eda097c3b8b631a25aff9
lunabaalbaki <83169533+lunabaalbaki@users.noreply.github.com>
2021-08-30 01:28:38 +0300

    Update app.py

--- app.py
+++ app.py
@@ -119,12 +119,11 @@
 
 import streamlit as st
 from multiapp import MultiApp
-from apps import home, data_stats # import your app modules here
+from apps import data_stats # import your app modules here
 
 app = MultiApp()
 
 # Add all your application here
-app.add_app("Home", home.app)
 app.add_app("Data Stats", data_stats.app)
 
 # The main app
