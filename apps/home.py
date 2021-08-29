02a9e8fc89d7b552e347814bce7ddade6b9123bc
lunabaalbaki <83169533+lunabaalbaki@users.noreply.github.com>
2021-08-30 01:17:22 +0300

    Update home.py

--- apps/home.py
+++ apps/home.py
@@ -1,18 +1,8 @@
 import streamlit as st
 
 def app():
+    st.title('Home')
 
-    ## Making an About Section
-    st.markdown(f"<h2 style='text-align:left; font-family:arial;' >{'<b>About</b>'} </h2>", unsafe_allow_html=True)
+    st.write('This is the `home page` of this multi-page app.')
 
-    st.subheader("""
-    Welcome! This Web App is a decision making tool intended to increase the accessibility of data-driven insights to key decision makers of a bank, primarily those in the marketing department. Although this particular application is based on a Portuguese bank's dataset, the template demonstrated here is transferrable to multiple use cases, within the Banking Industry or otherwise.
-            """)
-
-    st.write("")
-    st.write("")
-    st.write("")
-
-    st.write("""
-    Dataset Source: [Moro et al., 2014] S. Moro, P. Cortez and P. Rita. A Data-Driven Approach to Predict the Success of Bank Telemarketing. Decision Support Systems, In press, http://dx.doi.org/10.1016/j.dss.2014.03.001
-    """)
+    st.write('In this app, we will be building a simple classification model using the Iris dataset.')
